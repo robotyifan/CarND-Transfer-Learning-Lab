a7e2dc3d7170475ff3a4eccbeb2d159fc0fc2881
robotyifan <37556402+robotyifan@users.noreply.github.com>
2018-07-09 16:21:02 +0200

    Add files via upload

---
+++ lenet-traffic.py
@@ -0,0 +1,187 @@
+# Load pickled data
+import pickle
+import matplotlib.pyplot as plt
+import random
+from sklearn.utils import shuffle
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+from keras.datasets import cifar10
+from sklearn.model_selection import train_test_split
+
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# y_train.shape is 2d, (50000, 1). While Keras is smart enough to handle this
+# it's a good idea to flatten the array.
+y_train = y_train.reshape(-1)
+y_test = y_test.reshape(-1)
+
+# TODO: Fill this in based on where you saved the training and testing data
+
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.3, random_state=42, stratify = y_train)
+
+
+# TODO: Number of training examples
+n_train = len(X_train)
+
+# TODO: Number of validation examples
+n_validation = X_valid.shape[0]
+
+# TODO: Number of testing examples.
+n_test = X_test.shape[0]
+
+# TODO: What's the shape of an traffic sign image?
+image_shape = [X_train.shape[1],X_train.shape[2]]
+
+# TODO: How many unique classes/labels there are in the dataset.
+n_classes = len(set(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+### Feel free to use as many code cells as needed.
+
+# Visualizations will be shown in the notebook.
+
+
+index = random.randint(0, n_train)
+image = X_train[index].squeeze()
+
+plt.figure(figsize=(1,1))
+plt.imshow(image, cmap="gray")
+print(y_train[index])
+
+
+
+#rename X_train by x_train to protect the original dataset
+#x_train = X_train
+
+#for i in range(X_train.shape[0]):
+#    for j in range(X_train.shape[1]):
+#        for k in range(X_train.shape[2]):
+#            for m in range(X_train.shape[3]):
+#                X_train[i][j][k][m] = (X_train[i][j][k][m] - 128) / 128
+
+X_train,y_train = shuffle(X_train,y_train)
+
+
+
+EPOCHS = 10 
+BATCH_SIZE = 128
+
+def LeNet(x):    
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+    
+    # SOLUTION: Layer 1: Convolutional. Input = 32x32x3. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # SOLUTION: Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # SOLUTION: Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+    fc0   = flatten(conv2)
+    
+    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    # SOLUTION: Activation.
+    fc1    = tf.nn.relu(fc1)
+
+    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(84))
+    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    
+    # SOLUTION: Activation.
+    fc2    = tf.nn.relu(fc2)
+
+    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 10), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(10))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+    
+    return logits
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
+
+rate = 0.001
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y,logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+### Calculate and report the accuracy on the training and validation set.
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+with  tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = n_train
+    
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+index = []
+for i in range(5):
+    
+    num = random.randint(0, n_test)
+    index.append(num)
+    image = X_test[num].squeeze()
+
+    plt.figure(figsize=(2,2))
+    plt.imshow(image, cmap="gray")
+    print("Index: {}".format(num))
+    print("Label_test: {}".format(y_test[num]))
+  
